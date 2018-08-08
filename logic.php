31cd01b49c7d3230c26e3d89a01f9b752dc7cd12
Emanuil Nikolov <35072349+emanuilnikolov@users.noreply.github.com>
2018-08-08 23:24:36 +0300

    Finalized project - Added all required functionality

--- logic.php
+++ logic.php
@@ -1,2 +1,24 @@
 <?php
-echo $_SERVER['REQUEST_URI'];
+$param = urldecode(basename($_SERVER['REQUEST_URI']));
+
+try {
+    if (is_numeric($param)) {
+        $dt = new DateTime('@' . $param);
+        $unix = (int) $param;
+    } else {
+        $dt = new DateTime($param, new DateTimeZone('UTC'));
+        $unix = $dt->getTimestamp();
+    }
+    $date = $dt->format("F d, Y");
+} catch (Exception $e) {
+    $unix = null;
+    $date = null;
+}
+
+$result = [
+  'unix' => $unix,
+  'natural' => $date,
+];
+
+header('Content-type: application/json');
+echo json_encode($result);
