45d1a1b2657d0db14c0825cc1f5b14c575f8f82c
Emanuil <enikolov.intl@gmail.com>
2018-08-09 14:54:59 +0300

    Updated project with new requirements from freeCodeCamp.

--- api/timestamp/index.php
+++ api/timestamp/index.php
@@ -4,21 +4,23 @@ $param = urldecode(basename($_SERVER['REQUEST_URI']));
 try {
     if (is_numeric($param)) {
         $dt = new DateTime('@' . $param);
-        $unix = (int) $param;
+    } elseif (empty($param)) {
+        $dt = new DateTime('now');
     } else {
         $dt = new DateTime($param, new DateTimeZone('UTC'));
-        $unix = $dt->getTimestamp();
     }
-    $date = $dt->format("F d, Y");
-} catch (Exception $e) {
-    $unix = null;
-    $date = null;
-}
 
-$result = [
-  'unix' => $unix,
-  'natural' => $date,
-];
+    $unix = $dt->getTimestamp();
+    $date = $dt->format(DateTime::RFC7231);
+    $result = [
+      'unix' => $unix,
+      'utc' => $date,
+    ];
+} catch (Exception $e) {
+    $result = [
+      'error' => 'Invalid Date'
+    ];
+}
 
 header('Content-type: application/json');
 echo json_encode($result);
